f83ed2b6d982faffa0de6062ce64ae40066dc42d
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2023-12-01 19:46:06 -0800

    Create boilerplate file

---
+++ boilerplate.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("1.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+	}
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	defer file.Close()
+
+	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
+
+	fmt.Println(len(lines))
+}
